78fa260e235882da0a3cf1c1e21a4da0cfb03d74
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2011-02-25 10:27:09 -0500

    fixed up unpack

--- fidonet/apps/unpack.py
+++ fidonet/apps/unpack.py
@@ -2,7 +2,7 @@ import os
 
 import fidonet
 import fidonet.app
-import fidonet.message
+from fidonet.formats import *
 
 def next_message(dir, start=1):
     count=start
@@ -19,13 +19,19 @@ class App(fidonet.app.App):
     def create_parser(self):
         p = super(App, self).create_parser()
         p.add_option('-d', '--output-directory')
+        p.add_option('--disk', action='store_false',
+                dest='packed')
+        p.add_option('--packed', action='store_true',
+                dest='packed')
+
+        p.set_default('packed', True)
         return p
 
     def handle_args(self, args):
         gtotal = 0
 
         for pktfile in args:
-            pkt = fidonet.PacketFactory(fd=open(pktfile))
+            pkt = fidonet.PacketFactory(open(pktfile))
 
             count = 0
             while True:
@@ -33,7 +39,11 @@ class App(fidonet.app.App):
                     msg = fidonet.MessageFactory(pkt.messages)
                     msgfile = next_message(self.opts.output_directory)
                     fd = open(msgfile, 'w')
-                    fidonet.message.MessageParser.write(msg, fd)
+
+                    if self.opts.packed:
+                        packedmessage.MessageParser.write(msg,fd)
+                    else:
+                        diskmessage.MessageParser.write(msg, fd)
                     fd.close()
                     self.log.debug('wrote message from %s to %s' % (
                         pktfile, msgfile))
