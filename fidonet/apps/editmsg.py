5c78baebc71e0cf36406b585c820c45eef031645
Lars Kellogg-Stedman <lars@oddbit.com>
2011-02-26 22:17:40 -0500

    rewrote to use self.for_each_arg

--- fidonet/apps/editmsg.py
+++ fidonet/apps/editmsg.py
@@ -23,36 +23,39 @@ class App (fidonet.app.App):
         return p
 
     def handle_args(self, args):
+        self.for_each_arg(self.edit_msg, args)
 
-        for msgfile in args:
-            fd = open(msgfile, 'r+')
-            msg = fidonet.MessageFactory(fd=fd)
+    def edit_msg(self, src, name, ctx):
+        msg = fidonet.MessageFactory(src)
 
-            if self.opts.toname:
-                self.log.debug('setting to = %s' % self.opts.toname)
-                msg.toUsername = self.opts.toname
-            if self.opts.fromname:
-                self.log.debug('setting from = %s' % self.opts.fromname)
-                msg.fromUsername = self.opts.fromname
-            if self.opts.origin:
-                addr = fidonet.Address(self.opts.origin)
-                self.log.debug('setting origin = %s' % addr)
-                msg.origAddr = addr
-            if self.opts.destination:
-                addr = fidonet.Address(self.opts.destination)
-                self.log.debug('setting destination = %s' % addr)
-                msg.destAddr = addr
-            if self.opts.subject:
-                self.log.debug('setting subject = %s' % self.opts.subject)
-                msg.subject = self.opts.subject
+        if self.opts.toname:
+            self.log.debug('setting to = %s' % self.opts.toname)
+            msg.toUsername = self.opts.toname
+        if self.opts.fromname:
+            self.log.debug('setting from = %s' % self.opts.fromname)
+            msg.fromUsername = self.opts.fromname
+        if self.opts.origin:
+            addr = fidonet.Address(self.opts.origin)
+            self.log.debug('setting origin = %s' % addr)
+            msg.origAddr = addr
+        if self.opts.destination:
+            addr = fidonet.Address(self.opts.destination)
+            self.log.debug('setting destination = %s' % addr)
+            msg.destAddr = addr
+        if self.opts.subject:
+            self.log.debug('setting subject = %s' % self.opts.subject)
+            msg.subject = self.opts.subject
 
-            if self.opts.debug:
-                import pprint
-                pprint.pprint(msg)
+        if self.opts.debug:
+            import pprint
+            pprint.pprint(msg)
 
-            fd.seek(0)
-            fidonet.message.MessageParser.write(msg, fd)
-            fd.close()
+        if name == '<stdin>':
+            msg.write(sys.stdout)
+        else:
+            src.seek(0)
+            msg.write(src)
+            src.close()
 
 if __name__ == '__main__':
     App.run()
