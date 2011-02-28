f86b8415d28f3ffda523216d560ec421de1fa769
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2011-02-28 16:49:28 -0500

    ftn-route now uses new router class

--- fidonet/apps/route.py
+++ fidonet/apps/route.py
@@ -5,6 +5,7 @@ import os
 import fidonet
 import fidonet.app
 import fidonet.nodelist
+import fidonet.router
 from fidonet.nodelist import Node
 
 class App (fidonet.app.App):
@@ -12,9 +13,7 @@ class App (fidonet.app.App):
     def create_parser(self):
         p = super(App, self).create_parser()
         p.add_option('-n', '--nodelist')
-        p.add_option('-N', '--noroute', action='store_true')
-        p.add_option('-z', '--zone', default='1')
-        p.add_option('-I', '--internet', '--ina', action='store_true')
+        p.add_option('-r', '--routes')
         return p
 
     def handle_args(self, args):
@@ -22,35 +21,22 @@ class App (fidonet.app.App):
             nodelist = self.cfg.get('fidonet', 'nodelist').split()[0]
             self.opts.nodelist = os.path.join(
                     os.path.dirname(nodelist), 'nodeindex.db')
-
+        if self.opts.routes is None:
+            self.opts.routes = self.cfg.get('fidonet', 'routes')
 
         self.log.info('using nodelist %s' % self.opts.nodelist)
-        fidonet.nodelist.setup('sqlite:///%s' % self.opts.nodelist)
+        self.log.info('using routes from %s' % self.opts.routes)
+
+        router = fidonet.router.Router(self.opts.routes)
+
+        fidonet.nodelist.setup_nodelist('sqlite:///%s' % self.opts.nodelist)
         session = fidonet.nodelist.broker()
 
         for addr in args:
-            dst = fidonet.Address(addr)
-            print 'Finding route to', dst.ftn
-
-            node = session.query(Node).filter(Node.address==dst.ftn).first()
-
-            if self.opts.noroute:
-                hub = None
-                host = None
-            else:
-                hub = session.query(Node).filter(Node.net==dst.net).filter(Node.kw=='Hub').first()
-                host = session.query(Node).filter(Node.net==dst.net).filter(Node.node==0).first()
-
-            if hub is None and host is None and node:
-                print dst.ftn, 'DIRECT'
-            elif hub and (not self.opts.noroute or node and node.kw in ['Pvt', 'Hold']):
-                print dst.ftn, 'HUB', fidonet.Address(hub.address).ftn
-            elif host and (not self.opts.noroute or node and node.kw in ['Pvt', 'Hold']):
-                print dst.ftn, 'HOST', fidonet.Address(host.address).ftn
-            elif not node:
-                print dst.ftn, 'ORPHAN'
-            else:
-                print dst.ftn, 'DIRECT'
+            addr = fidonet.Address(addr)
+            rspec = router[addr]
+            print addr, 'via', rspec[0], 'using', ' '.join([str(x) for x in
+                rspec[1]])
 
 if __name__ == '__main__':
     App.run()
