ba5d0475de4099c2cfb7e251120118ad08c4decd
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2011-02-28 09:52:44 -0500

    using Boolean instead of Field

--- fidonet/formats/attributeword.py
+++ fidonet/formats/attributeword.py
@@ -4,24 +4,24 @@ packed and on-disk messages.
 .. _FTS-0001: http://www.ftsc.org/docs/fts-0001.016
 '''
 
-from fidonet.bitparser import Struct, Field, CString, BitStream
+from fidonet.bitparser import *
 
 AttributeWordParser = Struct(
-        Field('private', 'uint:1'),
-        Field('crash', 'uint:1'),
-        Field('received', 'uint:1'),
-        Field('sent', 'uint:1'),
-        Field('fileAttached', 'uint:1'),
-        Field('inTransit', 'uint:1'),
-        Field('orphan', 'uint:1'),
-        Field('killSent', 'uint:1'),
-        Field('local', 'uint:1'),
-        Field('holdForPickup', 'uint:1'),
-        Field('unused1', 'uint:1'),
-        Field('fileRequest', 'uint:1'),
-        Field('returnReceiptRequested', 'uint:1'),
-        Field('isReturnReceipt', 'uint:1'),
-        Field('auditRequest', 'uint:1'),
-        Field('fileUpdateRequest', 'uint:1'),
+        Boolean('private'),
+        Boolean('crash'),
+        Boolean('received'),
+        Boolean('sent'),
+        Boolean('fileAttached'),
+        Boolean('inTransit'),
+        Boolean('orphan'),
+        Boolean('killSent'),
+        Boolean('local'),
+        Boolean('holdForPickup'),
+        Boolean('unused1'),
+        Boolean('fileRequest'),
+        Boolean('returnReceiptRequested'),
+        Boolean('isReturnReceipt'),
+        Boolean('auditRequest'),
+        Boolean('fileUpdateRequest'),
         )
 
