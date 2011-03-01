605287c628ac4ff2d1705c6cf8e839c9f619378c
Lars Kellogg-Stedman <lars@oddbit.com>
2011-03-01 09:01:19 -0500

    make editmsg share most options with makemsg

--- fidonet/apps/editmsg.py
+++ fidonet/apps/editmsg.py
@@ -8,12 +8,24 @@ import fidonet
 import fidonet.app
 import fidonet.message
 
-class App (fidonet.app.AppUsingAddresses, fidonet.app.AppUsingNames):
+class App (fidonet.app.AppUsingFiles,
+        fidonet.app.AppUsingAddresses,
+        fidonet.app.AppUsingNames):
     logtag = 'fidonet.editmsg'
 
     def create_parser(self):
         p = super(App, self).create_parser()
+        p.add_option('-k', '--kludge', action='append', default=[])
         p.add_option('-s', '--subject')
+        p.add_option('-T', '--time')
+        p.add_option('-A', '--area')
+        p.add_option('-g', '--flag', action='append',
+                default=[])
+        p.add_option('--originline', '--oline')
+        p.add_option('--disk', action='store_false',
+                dest='packed')
+        p.add_option('--packed', action='store_true',
+                dest='packed')
         return p
 
     def handle_args(self, args):
@@ -39,12 +51,26 @@ class App (fidonet.app.AppUsingAddresses, fidonet.app.AppUsingNames):
         if self.opts.subject:
             self.log.debug('setting subject = %s' % self.opts.subject)
             msg.subject = self.opts.subject
+        if self.opts.time:
+            msg.dateTime = self.opts.time
+            self.log.debug('set dateTime = %s' % msg.dateTime)
+
+        for f in self.opts.flag:
+            if f.startswith('!'):
+                msg.attributeWord[f[1:]] = False
+            else:
+                msg.attributeWord[f] = True
+
+        if self.opts.area:
+            msg.body.area = self.opts.area
+
+        for k in self.opts.kludge:
+            k_name, k_val = k.split(' ', 1)
+            msg.body.klines[k_name] = msg.body.klines.get(k_name, []) + [k_val]
 
         if self.opts.debug:
             import pprint
-            pprint.pprint(msg)
-        else:
-            print msg
+            pprint.pprint(msg, stream=sys.stderr)
 
         if name == '<stdin>':
             msg.write(sys.stdout)
