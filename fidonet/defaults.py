2fe5cbd5d34fcc1ceb3c574c032d2e6de0f81b11
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2011-03-02 16:00:08 -0500

    added some diagnostics

--- fidonet/defaults.py
+++ fidonet/defaults.py
@@ -4,3 +4,10 @@ ftn_config_dir = os.environ.get('FTN_CONFIG_DIR', '/etc/ftn')
 ftn_config_file = os.path.join(ftn_config_dir, 'ftn.cfg')
 ftn_data_dir = os.environ.get('FTN_DATA_DIR', '/var/lib/ftn')
 
+if __name__ == '__main__':
+
+    for varname, varval in locals().items():
+        if varname.startswith('ftn_'):
+            print varname, '=', varval
+
+
