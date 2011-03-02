4a0f3b26a31c94628fe008430d33f37bcdb009e6
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2011-03-02 16:08:03 -0500

    ...and make it callable

--- fidonet/defaults.py
+++ fidonet/defaults.py
@@ -4,10 +4,11 @@ ftn_config_dir = os.environ.get('FTN_CONFIG_DIR', '/etc/ftn')
 ftn_config_file = os.path.join(ftn_config_dir, 'ftn.cfg')
 ftn_data_dir = os.environ.get('FTN_DATA_DIR', '/var/lib/ftn')
 
-if __name__ == '__main__':
-
-    for varname, varval in locals().items():
+def show_defaults():
+    for varname, varval in globals().items():
         if varname.startswith('ftn_'):
             print varname, '=', varval
 
+if __name__ == '__main__':
+    show_defaults()
 
