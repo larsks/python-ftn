7cc32b5356b41b82aadb26e381f4bcac6f810cc6
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2011-03-03 13:10:00 -0500

    upped version, minor formatting cleanup

--- fidonet/__init__.py
+++ fidonet/__init__.py
@@ -1,11 +1,12 @@
 from ftnerror import *
 
-__author__ = 'Lars Kellogg-Stedman'
+__author__  = 'Lars Kellogg-Stedman'
 __license__ = 'GPL'
-__email__ = 'lars@oddbit.com'
-__version__ = '1.1.0'
+__email__   = 'lars@oddbit.com'
+__version__ = '1.2.0'
 
 from packetfactory import PacketFactory
 from messagefactory import MessageFactory
 from address import Address
 from router import Router
+
