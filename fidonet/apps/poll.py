24d701f2f5f6921527335fd62cc49d455f461eb2
Lars Kellogg-Stedman <lars@oddbit.com>
2011-03-06 00:02:48 -0500

    made ftn-poll use binkd outbound by default and added mode flags

--- fidonet/apps/poll.py
+++ fidonet/apps/poll.py
@@ -1,3 +1,5 @@
+import os
+
 import fidonet
 import fidonet.app
 
@@ -16,17 +18,32 @@ class App (fidonet.app.App):
     If you have configured a path to your binkd outbound directory in
     ``fidonet.cfg``, ftn-poll will create poll files there; otherwise it
     will use your current directory.'''
+    
+    def create_parser(self):
+        p = super(App, self).create_parser()
+        p.add_option('-C', '--crash', action='store_const',
+                dest='mode', const='c')
+        p.add_option('-I', '--immediate', action='store_const',
+                dest='mode', const='i')
+        p.add_option('-D', '--direct', action='store_const',
+                dest='mode', const='d')
+        p.add_option('-F', '--normal', action='store_const',
+                dest='mode', const='f')
+
+        p.set_default('mode', 'f')
+        return p
 
     def handle_args(self, args):
-        try:
-            outb = self.cfg.get('binkd', 'outbound')
-        except:
+        outb = self.get('binkd', 'outbound')
+        if outb is None:
             outb = '.'
 
         for addr in args:
             addr = fidonet.Address(addr)
             self.log.info('creating poll for %s.' % addr)
-            open('%s.ilo' % addr.hex, 'w').close()
+            poll = os.path.join(outb, '%s.%slo' % (addr.hex, self.opts.mode))
+            self.log.debug('poll file = %s' % poll)
+            open(poll, 'w').close()
 
 if __name__ == '__main__':
     App.run()
