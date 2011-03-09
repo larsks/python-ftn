ef0ff3f6a1ade729ca6a4874ab5f5689abd823d5
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2011-03-09 15:14:53 -0500

    broken binkprobe script for further work

--- fidonet/apps/binkprobe.py
+++ fidonet/apps/binkprobe.py
@@ -1,12 +1,14 @@
 import os
 import sys
-import bitstring
 import socket
 import cPickle as pickle
 import time
+import struct
+import pprint
 
 import fidonet.app
 from fidonet.nodelist import Nodelist, Node, Flag
+from fidonet.binkp import BinkpConnection
 
 class ConnectionFailed (Exception):
     pass
@@ -22,6 +24,10 @@ class App (fidonet.app.App):
                 default='24554')
         p.add_option('-i', '--interval',
                 default='1')
+        p.add_option('-O', '--output', '--out')
+        p.add_option('--noincremental', action='store_true')
+        p.add_option('-N', '--noload', action='store_true')
+        p.add_option('-I', '--inet', action='store_true')
         return p
 
     def handle_args(self, args):
@@ -30,9 +36,10 @@ class App (fidonet.app.App):
             self.opts.nodelist = '%s.idx' % nodelist
 
         results = {}
-        output = args.pop(0)
-        if os.path.exists(output):
-            results = pickle.load(open(output))
+        if self.opts.output \
+                and not self.opts.noload \
+                and os.path.exists(self.opts.output):
+            results = pickle.load(open(self.opts.output))
 
         self.opts.port = int(self.opts.port)
         self.opts.interval = int(self.opts.interval)
@@ -57,34 +64,16 @@ class App (fidonet.app.App):
                 continue
 
             d = self.probe(n)
+            if self.opts.debug:
+                pprint.pprint(d)
+
             results[d['__ftnaddress__']] = d
-            pickle.dump(results, open(output, 'w'))
+            if self.opts.output and not self.opts.noincremental:
+                pickle.dump(results, open(self.opts.output, 'w'))
             time.sleep(self.opts.interval)
 
-        pickle.dump(results, open(output, 'w'))
-
-    def connect(self, node, ndinfo):
-        inet = node.inet('IBN')
-
-        if not inet:
-            raise ConnectionFailed('no internet address in nodelist' %
-                    node.address)
-
-        if ':' in inet:
-            addr, port = inet.split(':')
-        else:
-            addr, port = inet, self.opts.port
-
-        srvraddr = socket.gethostbyname(addr)
-
-        ndinfo['__inet__'] = inet
-        ndinfo['__inetaddr__'] = srvraddr
-
-        s = socket.socket()
-        s.settimeout(float(self.opts.timeout))
-        s.connect((srvraddr, int(port)))
-
-        return s
+        if self.opts.output:
+            pickle.dump(results, open(self.opts.output, 'w'))
 
     def probe(self, node):
         self.log.info('probing node %s' % node.address)
@@ -95,7 +84,6 @@ class App (fidonet.app.App):
         try:
             s = self.connect(node, ndinfo)
             self.read_binkp_info(s, node, ndinfo)
-
             s.close()
         except ConnectionFailed, detail:
             self.log.error('%s: connection failed: %s' % (node.address,
@@ -113,34 +101,84 @@ class App (fidonet.app.App):
                 detail))
             ndinfo['__failed__'] = str(detail)
         except Exception, detail:
-            self.log.error('%s: an unknown error occurred: %s' %
-                    (node.address, detail))
+            self.log.error('%s: an unknown error occurred: %s' % (
+                node.address, detail))
             ndinfo['__failed__'] = str(detail)
+
+            if self.opts.debug:
+                raise
 
         return ndinfo
 
+    def read_bytes(self, s, want):
+        bytes = s.recv(want)
+
+        while len(bytes) < want:
+            print 'want %d, got %d, reading %d' % (
+                    want, len(bytes), (want-len(bytes)))
+            bytes += s.recv(want - len(bytes))
+
+        return bytes
+
+    def read_frame (self, s):
+        bytes = self.read_bytes(s, 2)
+        frame_header = struct.unpack('>H', bytes)[0]
+        cmd_frame = frame_header & 0x8000
+        data_len = frame_header & ~0x8000
+
+        if cmd_frame:
+            cmd_id = struct.unpack('B', s.recv(1))[0]
+            data = self.read_bytes(s, data_len - 1)
+
+            self.log.debug('received command frame, cmd_id = %d, data = %s'
+                    % (cmd_id, data))
+        else:
+            cmd_id = 0
+            data = self.read_bytes(s, data_len)
+            self.log.debug('received data frame, length = %d bytes' %
+                    data_len)
+
+        return { 'command': bool(cmd_frame),
+                'cmd_id': cmd_id,
+                'data': data }
+
+    def send_cmd_frame(self, s, cmd_id, data):
+        data = struct.pack('b', cmd_id) + data
+        data_len = len(data)
+        frame_header = data_len | 0x8000
+
+        s.sendall(struct.pack('>H', frame_header))
+        s.sendall(data)
+
+        self.log.debug('sent command frame, cmd_id = %d, data = %s' %
+                (cmd_id, data))
+
     def read_binkp_info(self, s, node, ndinfo):
         while True:
-            bytes = s.recv(1) + s.recv(1)
-            bits = bitstring.BitStream(bytes=bytes)
+            frame = self.read_frame(s)
 
-            cmd = bits.read('bool')
-            len = bits.read('uint:15')
-
-            bits = bitstring.BitStream(bytes=s.recv(len))
-
-            if cmd:
-                cmdid = bits.read('uint:8')
-                if cmdid == 0:
-                    data = bits.read('bytes')
+            if frame['command']:
+                if frame['cmd_id'] == 0:
                     try:
-                        k,v = data.split(None, 1)
+                        k,v = frame['data'].split(None, 1)
                     except ValueError:
                         k = 'unknown_%d' % seq
                         v = data
                         seq += 1
-                    ndinfo[k] = v
+
+                    if k in ndinfo:
+                        ndinfo[k] = [ndinfo[k], v]
+                    else:
+                        ndinfo[k] = v
+                elif frame['cmd_id'] == 1:
+                    ndinfo['AKA'] = frame['data'].split()
+
+                    self.send_cmd_frame(s, 2, '-')
+                elif frame['cmd_id'] == 7:
+                    ndinfo['__passreq__'] = True
+                    break
                 else:
+                    self.log.debug('exit loop on cmdid = %d' % frame['cmd_id'])
                     break
 
 if __name__ == '__main__':
